8d8a70861149dad825b25b2c6ec809af11351321
Eric Vogt <eric.vogt.pro@gmail.com>
2025-09-10 20:13:17 +0200

    Error message translation

--- src/errors.rs
+++ src/errors.rs
@@ -1,5 +1,4 @@
 use serde::{Deserialize, Serialize};
-use crate::translation::{language_from, translate_to, LOCALES};
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
 pub enum Error {
@@ -7,11 +6,11 @@ pub enum Error {
 }
 
 impl Error {
-    fn translate_to(&self, lang_id: &str) -> String {
+    pub fn translate_to(&self, lang_id: &str) -> String {
         let message = match self {
             Error::TeamCreationError(message) => message,
         };
 
-        translate_to(lang_id, message)
+        crate::translation::translate_to(lang_id, message)
     }
 }
