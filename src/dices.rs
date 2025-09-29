a5b397e1ea13cab55d0a1694af5d043f126f5361
Eric Vogt <eric.vogt.pro@gmail.com>
2025-09-29 10:39:50 +0200

    dice rolling correction

--- src/dices.rs
+++ src/dices.rs
@@ -12,12 +12,12 @@ pub enum Dice {
 impl Dice {
     pub fn roll(self) -> usize {
         match self {
-            Dice::D2 => rand::rng().random_range(1..2),
-            Dice::D3 => rand::rng().random_range(1..3),
-            Dice::D6 => rand::rng().random_range(1..6),
+            Dice::D2 => rand::rng().random_range(1..=2),
+            Dice::D3 => rand::rng().random_range(1..=3),
+            Dice::D6 => rand::rng().random_range(1..=6),
             Dice::D6x2 => Dice::D6.roll() + Dice::D6.roll(),
-            Dice::D8 => rand::rng().random_range(1..8),
-            Dice::D16 => rand::rng().random_range(1..16),
+            Dice::D8 => rand::rng().random_range(1..=8),
+            Dice::D16 => rand::rng().random_range(1..=16),
         }
     }
 }
