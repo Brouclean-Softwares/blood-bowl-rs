ed9491f56168424bacc5dc671bcd13ffa15a692e
Eric Vogt <eric.vogt.pro@gmail.com>
2025-11-16 22:56:28 +0100

    Preparing v5s3

--- src/stars/v5s3.rs
+++ src/stars/v5s3.rs
@@ -7,7 +7,7 @@ pub(crate) fn star_position_list() -> Vec<Position> {
 }
 
 pub(crate) fn mega_star_position_list() -> Vec<Position> {
-    todo!()
+    Vec::new()
 }
 
 pub(crate) fn star_player_position_definition(position: &Position) -> Option<PositionDefinition> {
