370a4f855889ad72546a72a936f4cc132c58161f
Eric Vogt <eric.vogt.pro@gmail.com>
2026-05-25 16:39:43 +0200

    prayers bug fix

--- src/prayers/v5s3.rs
+++ src/prayers/v5s3.rs
@@ -1,7 +1,9 @@
+use crate::dices::Dice;
 use crate::prayers::PrayerToNuffle;
 
 pub fn roll_prayer() -> PrayerToNuffle {
-    super::v5::roll_prayer()
+    let dice_result = Dice::D16.roll();
+    prayers_list()[dice_result - 1].clone()
 }
 
 pub fn prayers_list() -> Vec<PrayerToNuffle> {
